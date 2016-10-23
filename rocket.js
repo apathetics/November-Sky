f17a0613a54b38204ad6c174d7257d9a26ba5355
Noah Simpson <noahdsimp@gmail.com>
2016-10-23 00:49:36 -0500

    created behavior type in data.json and added update methods for part and rocket js

--- rocket.js
+++ rocket.js
@@ -1,13 +1,17 @@
 class Rocket {
 	constructor() {
 		//needs container for parts
+		var partContainer = [];
 	}
 
 	add(part) {
 		//add part to a container
+		partContainer.push(part);
 	}
 
-	update() {
-		//update each part
+	update(dTime) {
+		for(i=0; i<partContainer.length; i++) {
+			partContainer[i].update(dTime);
+		}
 	}
 }
